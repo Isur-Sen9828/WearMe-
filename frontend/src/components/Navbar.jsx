e9da8bccae1d54ab804f383acefa2f4aff919d6a
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-08-21 19:44:46 +0530

    complete hearder and navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,13 +2,14 @@ import { NavLink } from "react-router-dom"
 import {MdCategory, MdContacts, MdHomeFilled, MdShop} from "react-icons/md"
 
 
+// eslint-disable-next-line react/prop-types
 const Navbar = ({containerStyles}) => {
   return (
 <nav className={`${containerStyles}`}>
-    <NavLink to={'/'}><div className="flexCenter gap-x-1"><MdHomeFilled/>Home</div></NavLink>
-    <NavLink to={'/mens'}><div className="flexCenter gap-x-1"><MdCategory/>Men</div></NavLink>
-    <NavLink to={'/womens'}><div className="flexCenter gap-x-1"><MdShop/>Women</div></NavLink>
-    <NavLink to={'/kids'}><div className="flexCenter gap-x-1"><MdContacts/>Kid's</div></NavLink>
+    <NavLink to={'/'}className={({isActive})=> isActive? "active_link" : ""}><div className="flexCenter gap-x-1"><MdHomeFilled/>Home</div></NavLink>
+    <NavLink to={'/mens'}className={({isActive})=> isActive? "active_link" : ""}><div className="flexCenter gap-x-1"><MdCategory/>Men</div></NavLink>
+    <NavLink to={'/womens'}className={({isActive})=> isActive? "active_link" : ""}><div className="flexCenter gap-x-1"><MdShop/>Women</div></NavLink>
+    <NavLink to={'/kids'}className={({isActive})=> isActive? "active_link" : ""}><div className="flexCenter gap-x-1"><MdContacts/>Kid's</div></NavLink>
 
 </nav>  
 
