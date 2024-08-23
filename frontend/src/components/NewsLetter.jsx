2ee90fab31daa8224263e62d7c28f68e461963bf
Isur-Sen9828 <95669455+Isur-Sen9828@users.noreply.github.com>
2024-08-23 12:44:08 +0530

    completed new collection and newsletter components

---
+++ frontend/src/components/NewsLetter.jsx
@@ -0,0 +1,17 @@
+
+function NewsLetter() {
+  return (
+    <section className="max_padd_container py-12 xl:py-28 bg-white">
+        <div className="mx-auto xl:w-[80%] flexCenter flex-col gap-y-8 w-full max-w-[660px]">
+            <h3 className="h3">Get Exclusve Offers On your Email</h3>
+            <h4 className="uppercase bold-18">Subscribe to our Newsletter and stay alert!</h4>
+            <div className="flexBetween rounded-full ring-1 ring-slate-900/5 hover:ring-slate-900/15 bg-primary w-full">
+                <input type="emal" placeholder="Enter your email address" className="w-full bg-transparent ml-7 border-none outline-none regular-16"/>
+                <button className="btn_dark_rounded">Subscribe</button>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default NewsLetter
